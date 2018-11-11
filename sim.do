5bcbf0ef2071087b45cb4829ceb6ce33f9246b78
Yashael Faith <yashael.faith@gmail.com>
2018-11-11 11:27:04 +0700

    newPerceptron test1

---
+++ sim.do
@@ -0,0 +1,5 @@
+add wave -r /*
+#force -freeze sim:/perceptron/i_k 16#01_000000_02_000000 0
+#force -freeze sim:/perceptron/i_w 16#00_800000_00_800000 0
+#force -freeze sim:/perceptron/i_b 16#00_400000 0
+run 100
--- src/perceptron.v
+++ src/perceptron.v
@@ -10,28 +10,29 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 
-module perceptron (i_k0, i_k1, i_w0, i_w1, i_bias, o_a, o_z);
+module perceptron (i_k, i_w, i_b, o);
 
 // parameters
+parameter NUM = 2;
 parameter WIDTH = 32;
 
 // input ports
-input signed [WIDTH-1:0] i_k0;
-input signed [WIDTH-1:0] i_k1;
-input signed [WIDTH-1:0] i_w0;
-input signed [WIDTH-1:0] i_w1;
-input signed [WIDTH-1:0] i_bias;
+input signed [NUM*WIDTH-1:0] i_k;
+input signed [NUM*WIDTH-1:0] i_w;
+input signed [WIDTH-1:0] i_b;
 
 // output ports
-output signed [WIDTH-1:0] o_a;
-output signed [WIDTH-1:0] o_z;
+output signed [WIDTH-1:0] o;
+// output signed [WIDTH-1:0] z;
 
 // wires
-wire signed [WIDTH-1:0] o_mult_0, o_mult_1;
+wire signed [WIDTH*NUM-1:0] o_mul;
+wire signed [WIDTH-1:0] o_add;
 
-mult_2in mult_0 (.i_a(i_w0), .i_b(i_k0), .o(o_mult_0));
-mult_2in mult_1 (.i_a(i_w1), .i_b(i_k1), .o(o_mult_1));
-adder_3in add (.i_a(o_mult_0), .i_b(o_mult_1), .i_c(i_bias), .o(o_z));
-sigmf sigmoid (.i(o_z), .o(o_a));
+mult_2in #(.WIDTH(WIDTH)) mult[NUM-1:0] (.i_a(i_k), .i_b(i_w), .o(o_mul));
+adder #(.NUM(NUM+1), .WIDTH(WIDTH)) add (.i({i_b, o_mul}), .o(o));
+// sigmf sigmoid (.i(o_add), .o(o));
+
+// assign z = o_add;
 
 endmodule
